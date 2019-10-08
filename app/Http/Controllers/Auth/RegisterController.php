afbc7b7a47d52251d66af37fbc78903760235f5c
edcamero <39476414+edcamero@users.noreply.github.com>
2019-10-08 08:48:24 -0500

    cambios con mary

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -54,6 +54,7 @@ class RegisterController extends Controller
             'nickname' => ['required', 'string', 'max:255','unique:users'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:6', 'confirmed'],
+            'altura'=>['requiere'],
         ]);
     }
 
