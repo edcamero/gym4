89e8e8dab113054cc124e1c527ed08eb14a44d92
Alejandra Carreño <52646044+ale199421@users.noreply.github.com>
2019-11-11 22:20:07 -0500

    home
    coloque mi pag hecha no hace ninguna interferencia con el desarrollo del panel admin :pig:

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,380 @@
-@extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                           ola {{ session('status') }}
-                        </div>
-                    @endif
+<!DOCTYPE html>
+<html lang="es">
+<head>
 
-                    EL gimnacio  gonorrea
-                </div>
-            </div>
-        </div>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Resort Inn Responsive , Smartphone Compatible web template , Samsung, LG, Sony Ericsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<link rel="stylesheet" href="css/chocolat.css" type="text/css" media="screen">
+<link href="css/easy-responsive-tabs.css" rel='stylesheet' type='text/css'/>
+<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" property="" />
+<link rel="stylesheet" href="css/jquery-ui.css" />
+<link href="css/es.css" rel="stylesheet" type="text/css" media="all" />
+<script type="text/javascript" src="js/modernizr-2.6.2.min.js"></script>
+<!--fonts-->
+<link href="//fonts.googleapis.com/css?family=Oswald:300,400,700" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Federo" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+<!--//fonts-->
+</head>
+<body>
+<!-- header -->
+
+	<div class="w3_navigation">
+		<div class="container">
+			<nav class="navbar navbar-default">
+				<div class="navbar-header navbar-left">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Palanca de navegacion</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<h1><a class="navbar-brand" href="index.php"> FIT <span>GYM</span><p class="logo_w3l_agile_caption">El Mejor Gimnasio</p></a></h1>
+				</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+					<nav class="menu menu--iris">
+						<ul class="nav navbar-nav menu__list">
+							<li class="menu__item"><a href="#about" class="menu__link scroll">Acerca de</a></li>
+							<li class="menu__item"><a href="#team" class="menu__link scroll">Equipo</a></li>
+						    <li class="nav-item">
+                                <a class="nav-link " href="{{ route('login') }}">Login</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">Registro</a>
+                                </li>
+                            @endif
+						</ul>
+					</nav>
+				</div>
+			</nav>
+
+		</div>
+	</div>
+<!-- //header -->
+		<!-- banner -->
+	<div id="home" class="w3ls-banner">
+		<!-- banner-text -->
+		<div class="slider">
+			<div class="callbacks_container">
+				<ul class="rslides callbacks callbacks1" id="slider4">
+					
+					<li>
+						<div class="w3layouts-banner-top w3layouts-banner-top1">
+							<div class="container">
+								<div class="agileits-banner-info">
+								<h4>Clases Personalizadas</h4>
+									<h3>Profesionales de Calidad</h3>
+										<p>Ven y disfruta con nosotros</p>
+									<div class="agileits_w3layouts_more menu__item">
+				<a href="#" class="menu__link" data-toggle="modal" data-target="#myModal">leer más</a>
+			</div>
+								</div>	
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="w3layouts-banner-top w3layouts-banner-top2">
+							<div class="container">
+								<div class="agileits-banner-info">
+								<h4>Conoce Nuestros Servicios</h4>
+								<h3>Instalaciones de Punta </h3>
+										<p>Obtenga Su Plan de Entrenamiento hoy</p>
+									<div class="agileits_w3layouts_more menu__item">
+											<a href="#" class="menu__link" data-toggle="modal" data-target="#myModal">leer más</a>
+										</div>
+								</div>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</div>
+			<div class="clearfix"> </div>
+			<!--banner Slider starts Here-->
+		</div>
+		    <div class="thim-click-to-bottom">
+				<a href="#about" class="scroll">
+					<i class="fa fa-long-arrow-down" aria-hidden="true"></i>
+				</a>
+			</div>
+	</div>	
+	<!-- //banner --> 
+<!--//Header-->
+<!-- //Modal1 -->
+
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog">
+						<!-- Modal1 -->
+							<div class="modal-dialog">
+							<!-- Modal content-->
+								<div class="modal-content">
+									<div class="modal-header">
+										<button type="button" class="close" data-dismiss="modal">&times;</button>
+										<h4>FIT
+  <span>GYM</span></h4>
+									<img src="images/1f.jpg" alt=" " class="img-responsive">
+										<h5>Sabemos lo que amas</h5>
+										<p>Nuestro compromiso social con los colombianos parte de nuestra filosofía, para FIT GYM , el ejercicio es el motor de una vida sana, que le permite al que lo practica, alcanzar sus metas y ser cada día mejor. Para una comunidad vulnerable, el deporte es clave en su desarrollo, por este motivo apoyamos diferentes equipos y causas sociales brindándoles los medios necesarios para su desarrollo deportivo, sea con espacios de entrenamiento en nuestras sedes, dotación y hasta acompañamiento médico.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+
+
+<!-- //banner-bottom -->
+<!-- /about -->
+
+ 	<div class="about-wthree" id="about">
+		  <div class="container">
+				   <div class="ab-w3l-spa">
+                            <h3 class="title-w3-agileits title-black-wthree">Acerca de nuestro GYM FIT
+</h3> 
+						   <p class="about-para-w3ls"> Propósito:
+Transformar la vida de nuestros clientes a través del entrenamiento físico, haciendo que se sientan mejor, más fuertes y saludables, mejorando su autoestima y alcanzando sus sueños.
+
+</p>
+						   <img src="images/abou.jpg" class="img-responsive" alt="Hair Salon">
+										
+                                      
+		          </div>
+		   	<div class="clearfix"> </div>
     </div>
 </div>
-@endsection
+ 	<!-- //about -->
+
+<!-- team -->
+<div class="advantages">
+<div class="team" id="team">
+	<div class="container">
+			<h3 class="title-w3-agileits title-black-wthree">Conoce Nuestro Grupo</h3>
+			<div id="horizontalTab">
+					<ul class="resp-tabs-list">
+					<li>
+						<img src="images/teams1.jpg" alt=" " class="img-responsive" />
+					</li>
+					<li>
+						<img src="images/teams2.jpg" alt=" " class="img-responsive" />
+					</li>
+					<li>
+						<img src="images/teams3.jpg" alt=" " class="img-responsive" />
+					</li>
+					<li>
+						<img src="images/teams4.jpg" alt=" " class="img-responsive" />
+					</li>
+					</ul>
+					<div class="resp-tabs-container">
+					<div class="tab1">
+						<div class="col-md-6 team-img-w3-agile">
+						</div>
+						<div class="col-md-6 team-Info-agileits">
+							<h4>Gregory James</h4>
+							<span>Gerente
+</span>
+							<p>El encargado de dirigir GYM FIT proactivo, serio e integro.
+						<div class="social-bnr-agileits footer-icons-agileinfo">
+							<ul class="social-icons3">
+								<li><a href="#" class="fa fa-facebook icon-border facebook"> </a></li>
+								<li><a href="#" class="fa fa-twitter icon-border twitter"> </a></li>
+								<li><a href="#" class="fa fa-google-plus icon-border googleplus"> </a></li> 
+								<li><a href="#" class="fa fa-rss icon-border rss"> </a></li>
+							</ul>
+						</div>							
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="tab2">
+					<div class="col-md-6 team-img-w3-agile">
+						</div>
+						<div class="col-md-6 team-Info-agileits">
+							<h4>Sarah Connor</h4>
+							<span>Entrenadora
+</span>
+							<p>La bella Sara te ayudara con sus clases de baile a encontrarte.</p>	
+						<div class="social-bnr-agileits footer-icons-agileinfo">
+							<ul class="social-icons3">
+								<li><a href="#" class="fa fa-facebook icon-border facebook"> </a></li>
+								<li><a href="#" class="fa fa-twitter icon-border twitter"> </a></li>
+								<li><a href="#" class="fa fa-google-plus icon-border googleplus"> </a></li> 
+								<li><a href="#" class="fa fa-rss icon-border rss"> </a></li>
+							</ul>
+						</div>							
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="tab3">
+						<div class="col-md-6 team-img-w3-agile">
+						</div>
+						<div class="col-md-6 team-Info-agileits">
+							<h4>Ivan Simpson</h4>
+							<span>Entrenador FIT</span>
+							<p>Con Ivan te daras cuenta que los limites no existen cuando hay pasion.</p>
+						<div class="social-bnr-agileits footer-icons-agileinfo">
+							<ul class="social-icons3">
+								<li><a href="#" class="fa fa-facebook icon-border facebook"> </a></li>
+								<li><a href="#" class="fa fa-twitter icon-border twitter"> </a></li>
+								<li><a href="#" class="fa fa-google-plus icon-border googleplus"> </a></li> 
+								<li><a href="#" class="fa fa-rss icon-border rss"> </a></li>
+							</ul>
+						</div>							
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="tab4">
+					<div class="col-md-6 team-img-w3-agile">
+						</div>
+						<div class="col-md-6 team-Info-agileits">
+							<h4>Marc Gutierrez</h4>
+							<span>Nutricionista</span>
+							<p>Mark es tu amigo te guia y te escucha .</p>
+						<div class="social-bnr-agileits footer-icons-agileinfo">
+							<ul class="social-icons3">
+								<li><a href="#" class="fa fa-facebook icon-border facebook"> </a></li>
+								<li><a href="#" class="fa fa-twitter icon-border twitter"> </a></li>
+								<li><a href="#" class="fa fa-google-plus icon-border googleplus"> </a></li> 
+								<li><a href="#" class="fa fa-rss icon-border rss"> </a></li>
+							</ul>
+						</div>							
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					</div>
+			</div>
+	</div>
+</div>
+	</div>
+<!-- //team -->
+
+	 
+<!-- /contact -->
+			<div class="copy">
+		        <p>© 2019  <a href="index.php">FIT GYM</a> </p>
+		    </div>
+<!--/footer -->
+<!-- js -->
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+<!-- contact form -->
+<script src="js/jqBootstrapValidation.js"></script>
+
+<!-- /contact form -->	
+<!-- Calendar -->
+		<script src="js/jquery-ui.js"></script>
+		<script>
+				$(function() {
+				$( "#datepicker,#datepicker1,#datepicker2,#datepicker3" ).datepicker();
+				});
+		</script>
+<!-- //Calendar -->
+<!-- gallery popup -->
+<link rel="stylesheet" href="css/swipebox.css">
+				<script src="js/jquery.swipebox.min.js"></script> 
+					<script type="text/javascript">
+						jQuery(function($) {
+							$(".swipebox").swipebox();
+						});
+					</script>
+<!-- //gallery popup -->
+<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- start-smoth-scrolling -->
+<!-- flexSlider -->
+				<script defer src="js/jquery.flexslider.js"></script>
+				<script type="text/javascript">
+				$(window).load(function(){
+				  $('.flexslider').flexslider({
+					animation: "slide",
+					start: function(slider){
+					  $('body').removeClass('loading');
+					}
+				  });
+				});
+			  </script>
+			<!-- //flexSlider -->
+<script src="js/responsiveslides.min.js"></script>
+			<script>
+						// You can also use "$(window).load(function() {"
+						$(function () {
+						  // Slideshow 4
+						  $("#slider4").responsiveSlides({
+							auto: true,
+							pager:true,
+							nav:false,
+							speed: 500,
+							namespace: "callbacks",
+							before: function () {
+							  $('.events').append("<li>before event fired.</li>");
+							},
+							after: function () {
+							  $('.events').append("<li>after event fired.</li>");
+							}
+						  });
+					
+						});
+			</script>
+		<!--search-bar-->
+		<script src="js/main.js"></script>	
+<!--//search-bar-->
+<!--tabs-->
+<script src="js/easy-responsive-tabs.js"></script>
+<script>
+$(document).ready(function () {
+$('#horizontalTab').easyResponsiveTabs({
+type: 'default', //Types: default, vertical, accordion           
+width: 'auto', //auto or any width like 600px
+fit: true,   // 100% fit in a container
+closed: 'accordion', // Start closed if in accordion view
+activate: function(event) { // Callback function if tab is switched
+var $tab = $(this);
+var $info = $('#tabInfo');
+var $name = $('span', $info);
+$name.text($tab.text());
+$info.show();
+}
+});
+$('#verticalTab').easyResponsiveTabs({
+type: 'vertical',
+width: 'auto',
+fit: true
+});
+});
+</script>
+<!--//tabs-->
+<!-- smooth scrolling -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+		/*
+			var defaults = {
+			containerID: 'toTop', // fading element id
+			containerHoverID: 'toTopHover', // fading element hover id
+			scrollSpeed: 1200,
+			easingType: 'linear' 
+			};
+		*/								
+		$().UItoTop({ easingType: 'easeOutQuart' });
+		});
+	</script>
+	
+	<div class="arr-w3ls">
+	<a href="#home" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+	</div>
+<!-- //smooth scrolling -->
+<script type="text/javascript" src="js/bootstrap-3.1.1.min.js"></script>
+</body>
+</html>
