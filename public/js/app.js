d194c9b332156fd62576e71ad868f9fd071e1967
edcamero <39476414+edcamero@users.noreply.github.com>
2019-12-03 18:21:04 -0500

    antes de morir

--- public/js/app.js
+++ public/js/app.js
@@ -2165,18 +2165,154 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
-  mounted: function mounted() {
-    console.log('Component mounted.');
-  },
   data: function data() {
     return {
-      saludo: ''
+      actual: '',
+      mes: '',
+      year: '',
+      month: '',
+      now: '',
+      last: '',
+      primerDiaSemana: '',
+      diaActual: '',
+      vectorMes: [],
+      semana: [],
+      dia: {
+        numero: ''
+      },
+      empleado: {
+        persona: {
+          tipo_doc: '',
+          documento: '',
+          nombre: '',
+          apellido: '',
+          fecha_nac: '',
+          sexo: '',
+          telefono: '',
+          direccion: '',
+          altura: '',
+          user: {
+            nickname: '',
+            email: '',
+            password: '',
+            password_confirmation: ''
+          }
+        },
+        tipoEmpleado: {
+          id: '',
+          nombre: ''
+        }
+      }
     };
+  },
+  created: function created() {
+    console.log("hola");
+    this.fechaActual();
   },
   methods: {
     hola: function hola() {
       console.log(this.saludo);
+    },
+    fechaActual: function fechaActual() {
+      this.actual = new Date();
+      this.year = this.actual.getFullYear();
+      this.month = this.actual.getMonth();
+      this.Calendario(this.year, this.month);
+    },
+    siguienteMes: function siguienteMes() {
+      this.month++;
+
+      if (this.month > 11) {
+        this.month = 0;
+        this.year++;
+      }
+
+      this.Calendario(this.year, this.month);
+    },
+    Calendario: function Calendario(year, month) {
+      this.GetMes(month);
+      var dia = 0;
+      this.diaActual = this.actual.getDate();
+      this.now = new Date(this.year, month, 1);
+      this.last = new Date(this.year, month + 1, 0);
+      this.primerDiaSemana = this.now.getDay();
+      var ultimoDiaMes = this.last.getDate();
+      var last_cell = this.primerDiaSemana + ultimoDiaMes;
+      console.log(ultimoDiaMes);
+      console.log(this.primerDiaSemana);
+      var dia = 1;
+      this.vectorMes = new Array(6);
+
+      for (var i = 0; i < 6; i++) {
+        this.vectorMes[i] = new Array(7);
+        var id = 1;
+
+        for (var j = 0; j < 7; j++) {
+          if (j == this.primerDiaSemana && i == 0 && dia == 1) {
+            this.vectorMes[i][j] = {
+              id: id++,
+              data: dia
+            };
+            dia++;
+            console.log(dia);
+            console.log(this.primerDiaSemana);
+          } else {
+            if (dia > 1 && dia <= ultimoDiaMes) {
+              this.vectorMes[i][j] = {
+                id: id++,
+                data: dia
+              };
+              dia++;
+            } else {
+              this.vectorMes[i][j] = {
+                id: id++,
+                data: ''
+              };
+            }
+          }
+        }
+      }
+    },
+    GetMes: function GetMes(m) {
+      var meses = Array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+      this.mes = meses[m];
     }
   }
 });
@@ -2192,54 +2328,6 @@ __webpack_require__.r(__webpack_exports__);
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 //
 //
 //
@@ -2366,6 +2454,9 @@ __webpack_require__.r(__webpack_exports__);
           _this2.empleados.splice(index, 1);
         });
       }
+    },
+    verHorario: function verHorario(empleado) {
+      location.href = '/Empleado/horario/';
     }
   }
 });
@@ -39719,24 +39810,86 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _vm._m(0)
+  return _c("div", { staticClass: "container" }, [
+    _c("div", { staticClass: "row justify-content-center" }, [
+      _c("div", { staticClass: "col-md-10" }, [
+        _c("div", { staticClass: "card" }, [
+          _c("div", { staticClass: "card-header" }, [_vm._v("Calendario")]),
+          _vm._v(" "),
+          _c("div", { staticClass: "card-body" }, [
+            _c("div", { staticClass: "row" }, [
+              _c("div", { staticClass: "col-10" }, [
+                _c("label", [
+                  _c("strong", [
+                    _vm._v(
+                      " " + _vm._s(this.mes) + " " + _vm._s(this.year) + "  "
+                    )
+                  ])
+                ])
+              ]),
+              _vm._v(" "),
+              _c("div", { staticClass: "col-2" }, [
+                _c(
+                  "button",
+                  {
+                    staticClass: "btn btn-success btn-sm",
+                    on: { click: _vm.siguienteMes }
+                  },
+                  [_c("i", { staticClass: "fas fa-arrow-circle-right" })]
+                )
+              ])
+            ]),
+            _vm._v(" "),
+            _c("table", { staticClass: "table table-sm" }, [
+              _vm._m(0),
+              _vm._v(" "),
+              _c(
+                "tbody",
+                _vm._l(this.vectorMes, function(semana, index) {
+                  return _c(
+                    "tr",
+                    { key: index },
+                    _vm._l(semana, function(dia) {
+                      return _c("td", { key: dia.id }, [
+                        dia.data == _vm.diaActual
+                          ? _c("div", { class: { "bg-info": true } }, [
+                              _vm._v(_vm._s(dia.data))
+                            ])
+                          : _c("div", [_vm._v(_vm._s(dia.data))])
+                      ])
+                    }),
+                    0
+                  )
+                }),
+                0
+              )
+            ])
+          ])
+        ])
+      ])
+    ])
+  ])
 }
 var staticRenderFns = [
   function() {
     var _vm = this
     var _h = _vm.$createElement
     var _c = _vm._self._c || _h
-    return _c("div", { staticClass: "container" }, [
-      _c("div", { staticClass: "row justify-content-center" }, [
-        _c("div", { staticClass: "col-md-8" }, [
-          _c("div", { staticClass: "card" }, [
-            _c("div", { staticClass: "card-header" }, [
-              _vm._v("Horario Empleado")
-            ]),
-            _vm._v(" "),
-            _c("div", { staticClass: "card-body" })
-          ])
-        ])
+    return _c("thead", [
+      _c("tr", [
+        _c("th", [_vm._v("Domingo")]),
+        _vm._v(" "),
+        _c("th", [_vm._v("Lunes ")]),
+        _vm._v(" "),
+        _c("th", [_vm._v("Martes ")]),
+        _vm._v(" "),
+        _c("th", [_vm._v("Miercoles")]),
+        _vm._v(" "),
+        _c("th", [_vm._v("Jueves")]),
+        _vm._v(" "),
+        _c("th", [_vm._v("Viernes")]),
+        _vm._v(" "),
+        _c("th", [_vm._v("Sabado ")])
       ])
     ])
   }
@@ -39838,9 +39991,10 @@ var render = function() {
                       }
                     ],
                     staticClass: "btn btn-success btn-sm",
-                    attrs: {
-                      "data-toggle": "modal",
-                      "data-target": "#ModalHorario"
+                    on: {
+                      click: function($event) {
+                        return _vm.verHorario(empleado)
+                      }
                     }
                   },
                   [_c("i", { staticClass: "far fa-calendar-alt" })]
@@ -39872,9 +40026,7 @@ var render = function() {
           0
         )
       ])
-    ]),
-    _vm._v(" "),
-    _vm._m(2)
+    ])
   ])
 }
 var staticRenderFns = [
@@ -39907,120 +40059,6 @@ var staticRenderFns = [
         _c("th", [_vm._v("Opciones")])
       ])
     ])
-  },
-  function() {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c(
-      "div",
-      {
-        staticClass: "modal",
-        attrs: {
-          id: "ModalHorario",
-          tabindex: "-1",
-          role: "dialog",
-          "aria-labelledby": "exampleModalLabel",
-          "aria-hidden": "true"
-        }
-      },
-      [
-        _c(
-          "div",
-          { staticClass: "modal-dialog", attrs: { role: "document" } },
-          [
-            _c("div", { staticClass: "modal-content" }, [
-              _c("div", { staticClass: "modal-header" }, [
-                _c(
-                  "h5",
-                  {
-                    staticClass: "modal-title",
-                    attrs: { id: "exampleModalLabel" }
-                  },
-                  [_vm._v("Horario del Empleado")]
-                ),
-                _vm._v(" "),
-                _c(
-                  "button",
-                  {
-                    staticClass: "close",
-                    attrs: {
-                      type: "button",
-                      "data-dismiss": "modal",
-                      "aria-label": "Close"
-                    }
-                  },
-                  [
-                    _c("span", { attrs: { "aria-hidden": "true" } }, [
-                      _vm._v("×")
-                    ])
-                  ]
-                )
-              ]),
-              _vm._v(" "),
-              _c("div", { staticClass: "modal-body" }, [
-                _c("div", { staticClass: "form-group" }),
-                _vm._v(" "),
-                _c("table", { staticClass: "table table-sm" }, [
-                  _c("thead", [
-                    _c("tr", [
-                      _c("th", [_vm._v("Domingo")]),
-                      _vm._v(" "),
-                      _c("th", [_vm._v("Lunes ")]),
-                      _vm._v(" "),
-                      _c("th", [_vm._v("Martes ")]),
-                      _vm._v(" "),
-                      _c("th", [_vm._v("Miercoles")]),
-                      _vm._v(" "),
-                      _c("th", [_vm._v("Jueves")]),
-                      _vm._v(" "),
-                      _c("th", [_vm._v("Viernes")]),
-                      _vm._v(" "),
-                      _c("th", [_vm._v("Sabado ")])
-                    ])
-                  ]),
-                  _vm._v(" "),
-                  _c("tbody", [
-                    _c("tr", [
-                      _c("td"),
-                      _vm._v(" "),
-                      _c("td"),
-                      _vm._v(" "),
-                      _c("td"),
-                      _vm._v(" "),
-                      _c("td"),
-                      _vm._v(" "),
-                      _c("td"),
-                      _vm._v(" "),
-                      _c("td"),
-                      _vm._v(" "),
-                      _c("td")
-                    ])
-                  ])
-                ])
-              ]),
-              _vm._v(" "),
-              _c("div", { staticClass: "modal-footer" }, [
-                _c(
-                  "button",
-                  {
-                    staticClass: "btn btn-secondary",
-                    attrs: { type: "button", "data-dismiss": "modal" }
-                  },
-                  [_vm._v("Cerrar")]
-                ),
-                _vm._v(" "),
-                _c(
-                  "button",
-                  { staticClass: "btn btn-primary", attrs: { type: "button" } },
-                  [_vm._v("Guardar")]
-                )
-              ])
-            ])
-          ]
-        )
-      ]
-    )
   }
 ]
 render._withStripped = true
@@ -55736,8 +55774,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\gym4\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\gym4\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\blade\OneDrive\Desktop\gym4\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\blade\OneDrive\Desktop\gym4\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container ">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                           ola {{ session('status') }}
-                        </div>
-                    @endif
-
-                    ud ha ingresado!!!
-                </div>
-            </div>
+           
+            <img src="{{ asset('images/logo.png') }}" alt="" width="550" height="550">
+            
         </div>
     </div>
 </div>
